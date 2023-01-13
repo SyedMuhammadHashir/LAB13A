3f118998be6f65386e0610ba293b549a1f7ed4f6
SyedMuhammadHashir <122075818+SyedMuhammadHashir@users.noreply.github.com>
2023-01-13 09:37:37 +0500

    FIRST COMMIT

---
+++ pRINTER.java
@@ -0,0 +1,46 @@
+package hashir567;
+
+public class pRINTER {
+	int page=10; 
+	 
+	synchronized void withdraw(int page){ 
+	System.out.println("going to printer"); 
+	 
+	if(this.page<page){ 
+	System.out.println("Less page;waiting for page "); 
+	try{wait();}catch(Exception e){} 
+	} 
+	this.page-=page; 
+	System.out.println("print complete"); 
+	} 
+	 
+	synchronized void deposit(int page){ 
+	System.out.println("going to printer"); 
+	this.page+=page; 
+	System.out.println("printer completed... "); 
+	notify(); 
+	} 
+	} 
+class Thread1 extends Thread
+{
+pRINTER c=new pRINTER (); 
+public void run()
+{
+c.withdraw(15);
+}
+}
+class Thread2 extends Thread
+{
+	pRINTER  c=new pRINTER (); 
+public void run()
+{
+c.deposit(10);
+}
+}
+class Use{ 
+public static void main(String args[]){ 
+Thread1 t1=new Thread1();
+Thread2 t2=new Thread2();
+t1.start();
+t2.start();
+}}
